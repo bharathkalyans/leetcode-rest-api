a10a99d1dff1a31a3f855a1c829447cfe5969f9e
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-21 15:27:09 +0530

    Added apollo server

---
+++ FakeData.js
@@ -0,0 +1,8 @@
+export let users = [
+  { name: "Bharath", age: 21, married: true },
+  { name: "Ananth", age: 21, married: false },
+  { name: "Supriya", age: 21, married: true },
+  { name: "Ram", age: 21, married: true },
+  { name: "Samanth", age: 21, married: false },
+  { name: "Rachael", age: 21, married: true },
+];
--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-const express = require("express");
+import express, { json } from "express";
+import cors from 'cors';
+import { users } from "./FakeData.js";
+
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.use(json(), cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
