ba83c18357e7b182f1c33b361bd3b887d4c9d9a3
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-22 12:33:13 +0530

    Express JS Set Up is Complete

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema/index.js";
 import { resolvers } from "./resolvers/index.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+import bodyParser from "body-parser";
+import express from "express";
 
 const PORT = process.env.PORT || 4000;
-
 dotenv.config();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
 
-const main = async () => {
-  const { url } = await startStandaloneServer(server, {
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/graphql",
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
-    listen: { port: PORT },
-  });
+  })
+);
 
-  console.log(`🚀 Server ready at ${PORT}, ${url}`);
-  console.log(`Query at : ${"https://studio.apollographql.com/dev"}`);
-};
-
-main();
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
