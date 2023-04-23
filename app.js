8e7795c11ca0a11b254f907de89374edbf97fc5f
Bharath Kalyan S <sbharath35@gmail.com>
2023-04-23 10:10:21 +0530

    Removed Page Design Code

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import router from "./routes/userInfo.js";
+import userRouter from "./routes/userInfo.js";
 import leetcodeRouter from "./routes/leetcode.js";
-import expressListRoutes from "./listRoutes.js";
-import path from 'path';
 
 dotenv.config();
 
@@ -14,25 +12,9 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors(), bodyParser.json());
 
-
 app.use("/leetcode", leetcodeRouter);
 
-app.use("/user", router);
-
-const allRoutes = expressListRoutes(app).map(x => {
-  return {
-    method: x.method,
-    path: x.path.replaceAll('\\', '/')
-  }
-});
-
-app.get('/', async (req, res) => {
-  res.sendFile(path.join(path.resolve(), './static/routes.html'));
-});
-
-app.get("/list-routes", async (req, res) => {
-  res.send(allRoutes);
-});
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is started at PORT : ${PORT}`);
